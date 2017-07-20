7997cbf7ab762d1a2487607a0facd4ccfae33a66
herepremg@gmail.com <herepremg@gmail.com>
2017-07-20 23:36:33 +0530

    footer social link added

--- include/footer.php
+++ include/footer.php
@@ -2,26 +2,31 @@
 <div class="container-fluid footer-fluid-container">
 
   <div class="container footer-container">
-    <!--  <div class="row footer-content-row">
+  <div class="row footer-content-row">
       <div class="col-md-3 footer-content-column">
-        <a href="https://www.textlocal.in"><img class="footer-logo" style="width:42px;" src="asset/images/logo-icon.png"  /></a>
-        <p>
+        <a href="https://www.textlocal.in"><img class="footer-logo" style="" src="asset/images/logo-icon.png"  /></a>
+        <!--<p>
           transactionalSMS by
           <a href="#"><strong>Textlocal</strong></a>
-        </p>
+        </p>!-->
+        </div>
+        <div class="col-md-3 footer-content-column">
+          <a href="mailto:support@textlocal.in">support@textlocal.in</a>
+        </div>
+        <div class="col-md-3 footer-content-column">
+          <a href="tel:+91-40-30858602">+91 40 30858602</a>
+        </div>
 
-        <p>
-          Phone : +91 40 30858602
-        </p>
-        <p>
-          Email : support@textlocal.in
-        </p>
-        <h3>
-          <a class="footer-social-link" href="#"><i class="fa fa-facebook  footer-faceboook"></i></a>
-          <a class="footer-social-link" href="#"><i class="fa fa-twitter  footer-twitter"></i></a>
-          <a class="footer-social-link" href="#"><i class="fa fa-youtube footer-youtube"></i></a>
-        </h3>
+      <div class="col-md-3 footer-content-column">
+
+          <a class="footer-social-link" href="https://www.facebook.com/transactionalsmscom/"><i class="fa fa-facebook footer-facebook"></i></a>
+          <a class="footer-social-link" href="https://plus.google.com/102338663400060845239"><i class="fa fa-google-plus footer-googleplus"></i></a>
+          <a class="footer-social-link" href="https://twitter.com/transactionsms"><i class="fa fa-twitter footer-twitter"></i></a>
+          <a class="footer-social-link" href="https://www.youtube.com/channel/UCUmQreBSGm86w3A1I9_cgdQ"><i class="fa fa-youtube footer-youtube"></i></a>
+
       </div>
+    </div>
+        <!--
       <div class="col-md-3 footer-content-column">
         <h5 class="footer-link-header">Plans</h5>
         <p class="footer-link"><a href="#">Promotional SMS</a></p>
@@ -44,12 +49,12 @@
     </div>
   !-->
     <div class="footer-copyright-row">
-      <div class="col-md-4">
+      <div class="col-md-4 footer-content-column">
         <p>
           © copyright 2017 <a href="https://imimobile.com/?tlrx=transactionalsms.com#">IMImobile</a>. All rights reserved.
         </p>
       </div>
-      <div class="col-md-8 text-right ">
+      <div class="col-md-8 footer-content-column">
         <a class="footer-copyright-link" href="transactionalsms-vs-promotionalsms">Transactional SMS vs Promotional SMS</a>
         <a class="footer-copyright-link" href="terms">Terms</a>
         <a class="footer-copyright-link" href="privacy">Privacy</a>
