f4a05cda81fb1c4b5293ec21f8a4de68a7856c82
Sai Vamsi <shade-slayer@users.noreply.github.com>
2017-08-24 11:20:18 +0530

    added GTM code

--- include/header.php
+++ include/header.php
@@ -10,7 +10,14 @@
   <link rel="icon" sizes="192x192" href="asset/images/transactional-sms-logo.svg">
   <meta name="keywords" content="transactional sms,transactional sms india, transactional sms api,sms sender ID" />
 
-
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P6GCX8B');</script>
+<!-- End Google Tag Manager -->
+	
   <!-- Font Family Embedded !-->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
